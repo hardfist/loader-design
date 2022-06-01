d0d148cdc67620d181a1edf491fbb801458cbc05
hardfist <yangjianzju@gmail.com>
2022-06-01 22:10:52 +0800

    chore: add parcel

---
+++ index.js
@@ -0,0 +1,38 @@
+var $9NSC0$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+
+
+
+const $9cfcb86616cc5a81$var$SvgLogo = (props)=>/*#__PURE__*/ (0, $9NSC0$reactjsxruntime.jsx)("svg", {
+        width: 33,
+        height: 33,
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg",
+        ...props,
+        children: /*#__PURE__*/ (0, $9NSC0$reactjsxruntime.jsxs)("g", {
+            clipPath: "url('#clip0')",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            children: [
+                /*#__PURE__*/ (0, $9NSC0$reactjsxruntime.jsx)("path", {
+                    d: "m5.378 16.98 7.372-7.55a5.096 5.096 0 0 1 7.292 0l.08.082a5.226 5.226 0 0 1 0 7.303l-7.372 7.55a5.096 5.096 0 0 1-7.292 0l-.08-.083a5.226 5.226 0 0 1 0-7.302Z",
+                    fill: "#12D2AC"
+                }),
+                /*#__PURE__*/ (0, $9NSC0$reactjsxruntime.jsx)("path", {
+                    d: "m20.048 9.43 7.292 7.467a5.344 5.344 0 0 1 0 7.467 5.096 5.096 0 0 1-7.292 0l-7.292-7.467a5.344 5.344 0 0 1 0-7.467 5.096 5.096 0 0 1 7.292 0Z",
+                    fill: "#307AF2"
+                }),
+                /*#__PURE__*/ (0, $9NSC0$reactjsxruntime.jsx)("path", {
+                    d: "m20.132 9.522 3.553 3.638-7.292 7.467-7.292-7.467 3.553-3.638a5.226 5.226 0 0 1 7.478 0Z",
+                    fill: "#0057FE"
+                })
+            ]
+        })
+    });
+var $9cfcb86616cc5a81$export$2e2bcd8739ae039 = $9cfcb86616cc5a81$var$SvgLogo;
+
+
+console.log("xxx", (0, $9cfcb86616cc5a81$export$2e2bcd8739ae039), tab, xxx);
+
+
+//# sourceMappingURL=index.js.map
